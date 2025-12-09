b58721c2e9d190c727c85a610e7ecf47290c963a
Fatima Azzahro <azzahro.fatima@gmail.com>
2025-12-09 14:19:58 +0700

    Variables - Add Double and Print it in Long Float and Maximum format

--- 002_variables.c
+++ 002_variables.c
@@ -6,11 +6,16 @@ int main()
     int age = 17;
     float gpa = 3.75;
     char grade = 'A';
+    double pi = 3.141592653589793;
 
     // Printing variable values
     printf("Age: %d\n", age);
     printf("GPA: %.2f\n", gpa); // Format to 2 decimal places
     printf("Grade: %c\n", grade);
+
+    printf("\n");
+    printf("Pi: %lf\n", pi);   // Print Double in Long Float format (6 digit)
+    printf("Pi: %.15f\n", pi); // Print Double in Max. Double precision (15 digit)
 
     return 0;
 }

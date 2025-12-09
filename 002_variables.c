6668129438a7cce86c59e4394ee4286c41cbb97d
Fatima Azzahro <azzahro.fatima@gmail.com>
2025-12-09 14:12:00 +0700

    Variables - Declare and Print Variables

---
+++ 002_variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaring variables
+    int age = 17;
+    float gpa = 3.75;
+    char grade = 'A';
+
+    // Printing variable values
+    printf("Age: %d\n", age);
+    printf("GPA: %f\n", gpa); // Format to 2 decimal places
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}

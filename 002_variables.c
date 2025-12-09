efbbb1805da2de95635e95ab3b636f0c8235676c
Fatima Azzahro <azzahro.fatima@gmail.com>
2025-12-10 06:22:08 +0700

    Variables - Declare and Print a String

--- 002_variables.c
+++ 002_variables.c
@@ -3,12 +3,14 @@
 int main()
 {
     // Declaring variables
+    char name[] = "Fatima Zza";
     int age = 17;
     float gpa = 3.75;
     char grade = 'A';
     double pi = 3.141592653589793;
 
     // Printing variable values
+    printf("Name: %s\n", name);
     printf("Age: %d\n", age);
     printf("GPA: %.2f\n", gpa); // Format to 2 decimal places
     printf("Grade: %c\n", grade);

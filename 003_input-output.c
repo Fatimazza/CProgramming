526b530b3b36495afa58af8aaeb5439eacf32c56
Fatima Azzahro <azzahro.fatima@gmail.com>
2025-12-09 18:58:05 +0700

    Input Output - Input then Print a String without any space

--- 003_input-output.c
+++ 003_input-output.c
@@ -3,9 +3,14 @@
 int main()
 {
     int num;
+    char str[100];
 
     printf("Input a number: ");
     scanf("%d", &num); // Read an integer from user input
 
+    printf("Input a String: ");
+    scanf("%s", str); // Read a string from user input
+
     printf("You entered: %d\n", num); // Print the entered number
+    printf("You typed: %s\n", str);   // Print the entered string
 }

1f521fa364654ad435bca1c033058c361947f95b
Fatima Azzahro <azzahro.fatima@gmail.com>
2025-12-09 18:50:35 +0700

    Input Output - Input then Print an Integer

---
+++ 003_input-output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Input a number: ");
+    scanf("%d", &num); // Read an integer from user input
+
+    printf("You entered: %d\n", num); // Print the entered number
+}

6b432535bb72a19bea9b24ab62bf07dd7564fe7e
Fatima Azzahro <azzahro.fatima@gmail.com>
2025-12-10 16:52:54 +0700

    Struct - Declare and Initialize Struct in run time

--- 010_struct.c
+++ 010_struct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct employee
 {
@@ -11,8 +12,16 @@ int main()
     // Initialization #1 in compile time
     struct employee info = {1, "Fatima Zza"};
 
+    // Initialization #2 in run time
+    struct employee info2;
+    strcpy(info2.name, "Fitimi Zzi");
+    info2.id = 2;
+
     printf("Employee Name: %s\n", info.name);
     printf("Employee ID  : %04d\n", info.id); // Output: 0001
+
+    printf("Employee Name: %s\n", info2.name);
+    printf("Employee ID  : %04d\n", info2.id); // Output: 0002
 
     return 0;
 }
